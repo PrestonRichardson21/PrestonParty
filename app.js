6a0ac8dd1f236bbe195d8690f82d1bd38d37ab0f
Preston Richardson <167050211+PrestonRichardson21@users.noreply.github.com>
2024-09-30 18:00:31 -0500

    Update app.js
    added scores

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 
-document.getElementById('spinButton').addEventListener('click', function() {
-    const result = spin();
-    document.getElementById('resultTextbox').innerText = result;
-});
+import { badCardLibrary, nuetralCardLibrary, goodCardLibrary } from './tatbCardLibrary.js';
 
-const spin = () =>{
-    let ran = Math.random()*10;
+     
+let spin = () =>{
+    let result = Math.floor(Math.random()*10 + 1);
+    console.log(result);
+    document.getElementById("spinResult").innerHTML = "They are a " + result + " but...";
+    let pick = Math.floor(Math.random() * 3);
 
-    return Math.ceil(ran);
+    if (result >= 1 && result <= 3) {
+        document.getElementById("cardResult").innerHTML = goodCardLibrary[pick];
+    } else if (result >= 4 && result <= 7) {
+        document.getElementById("cardResult").innerHTML = nuetralCardLibrary[pick];
+    } else {
+        document.getElementById("cardResult").innerHTML = badCardLibrary[pick];
+    }
+    // count++
+};
 
+let score =(id)=>{
+    document.getElementById(id).innerHTML++
 }
 
-// console.log(spin());
+function goToPage() {
+    window.location.href = "./tatb.html";
+}
+export { spin, score, goToPage };
