d35f7dd95e22034b1d973113f8564ebfbb83c97d
Preston Richardson <167050211+PrestonRichardson21@users.noreply.github.com>
2024-10-02 22:24:10 -0500

    Update app.js
    Added new pages

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { badCardLibrary, nuetralCardLibrary, goodCardLibrary } from './tatbCardL
 
      
 let spin = () =>{
+
     let result = Math.floor(Math.random()*10 + 1);
     console.log(result);
     document.getElementById("spinResult").innerHTML = "They are a " + result + " but...";
@@ -15,14 +16,23 @@ let spin = () =>{
     } else {
         document.getElementById("cardResult").innerHTML = badCardLibrary[pick];
     }
-    // count++
+    
+    
 };
 
 let score =(id)=>{
     document.getElementById(id).innerHTML++
 }
 
-function goToPage() {
-    window.location.href = "./tatb.html";
+function goToPage(directory) {
+    window.location.href = `${directory}`;
 }
-export { spin, score, goToPage };
+function showRules() {
+    alert(`
+         1. Whoever went on a date most recently serves as the first host. The host spins first. 
+         2. In their head the host picks the new number they would give that partner based on how much they value/hate the trait in question.
+         3. Once the host is ready, they count down from three. Using their fingers, everyone must then say what number they would give the potnetial partner.
+         4. Everyone who matches the host gets a point.
+         5. Whoever gets ten points first wins!`);
+}
+export { spin, score, goToPage, showRules };
